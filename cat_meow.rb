69dc04d6e761634433e731049d3471908dae8468
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-08-14 15:27:55 -0400

    fixed the cats, added am/pm

--- cat_meow.rb
+++ cat_meow.rb
@@ -13,17 +13,30 @@ class Cat
     @preferred_food
   end
 
-  def meal_time
-    @meal_time
-  end
+  def meal_time_calc
+    time = 0
+    if @meal_time > 12
+      time = @meal_time - 12
+      return "#{time}am"
+
+
+    else
+      time = @meal_time
+      return "#{time}pm"
+    end
+end
+
+
+
+
 
   def meow
-    "I'm #{@name} my favorite food is #{@preferred_food}. Also, i usually eat around #{@meal_time}"
+    "I'm #{@name} my favorite food is #{@preferred_food}. Also, i usually eat around #{meal_time_calc}"
   end
 
 end
 
-sniffy = Cat.new("sniffy", "chocolate", 6)
+sniffy = Cat.new("sniffy", "chocolate", 14)
 charlie = Cat.new("charlie", "cake", 7)
 ron = Cat. new("ron", "coke", 1)
 
