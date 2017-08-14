ef2af3cfc547a13a7dd858149fc3a6ced0999b88
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-08-14 14:08:31 -0400

    finished exercise1 bank account

--- bank_account.rb
+++ bank_account.rb
@@ -1,1 +1,40 @@
 class BankAccount
+  def initialize
+    @balance = 0
+    @interest_rate = 0.05
+  end
+
+  def balance
+    @balance
+  end
+
+  def interest_rate
+    @interest_rate
+  end
+
+  def deposit(deposit_amount)
+    @balance += deposit_amount
+  end
+
+  def withdraw(withdraw_amount)
+    @balance -= withdraw_amount
+  end
+
+  def gain_interest(gain_interest_inc)
+    @balance *= (gain_interest_inc + 1)
+  end
+end
+
+account1 = BankAccount.new
+
+account1.deposit(50)
+
+p account1
+
+account1.withdraw(20)
+
+p account1
+
+account1.gain_interest(0.05)
+
+p account1
